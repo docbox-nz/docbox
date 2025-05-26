dec2637fa41e2eac057dd6386afb62eccca7565b
Jacobtread <jacobtread@gmail.com>
2025-05-26 23:52:08 +1200

    fix: add parent id and processing config to base migration

--- packages/docbox-database/src/migrations/m9_create_presigned_upload_tasks_table.sql
+++ packages/docbox-database/src/migrations/m9_create_presigned_upload_tasks_table.sql
@@ -9,5 +9,11 @@ CREATE TABLE IF NOT EXISTS "docbox_presigned_upload_tasks" (
     "file_key" VARCHAR NOT NULL,
     "created_at" timestamp with time zone NOT NULL,
     "expires_at" timestamp with time zone NOT NULL,
-    "created_by" VARCHAR
+    "created_by" VARCHAR,
+    "parent_id" UUID
+        CONSTRAINT "FK_presigned_task_file"
+            REFERENCES "docbox_files" ("id")
+            ON DELETE SET NULL,
+    "processing_config" JSONB
 );
+
