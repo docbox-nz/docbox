05b878dd1b91d257251af0b3cb0c71c731003bee
Jacobtread <jacobtread@gmail.com>
2025-06-08 18:34:12 +1200

    feat: link delete test and check for lifecycle events

--- packages/docbox-core/tests/link.rs
+++ packages/docbox-core/tests/link.rs
@@ -1,19 +1,25 @@
 use docbox_core::{
     document_box::create_document_box::{CreateDocumentBox, create_document_box},
-    events::{TenantEventPublisher, noop::NoopEventPublisher},
-    links::create_link::{CreateLinkData, safe_create_link},
+    events::{TenantEventMessage, TenantEventPublisher, mpsc::MpscEventPublisher},
+    links::{
+        create_link::{CreateLinkData, safe_create_link},
+        delete_link::delete_link,
+    },
 };
+use docbox_database::models::link::Link;
 use docbox_search::models::{SearchIndexType, SearchRequest};
 
 use crate::common::{database::create_test_tenant_database, search::create_test_tenant_typesense};
 
 mod common;
 
+/// Tests that a link can be created successfully
 #[tokio::test]
 async fn test_create_link_success() {
     let (_container_db, db) = create_test_tenant_database().await;
     let (_container_search, search) = create_test_tenant_typesense().await;
-    let events = TenantEventPublisher::Noop(NoopEventPublisher);
+    let (events, mut events_rx) = MpscEventPublisher::new();
+    let events = TenantEventPublisher::Mpsc(events);
     let (document_box, root) = create_document_box(
         &db,
         &events,
@@ -24,6 +30,9 @@ async fn test_create_link_success() {
     )
     .await
     .unwrap();
+
+    // Consume creation event
+    _ = events_rx.recv().await.unwrap();
 
     let link = safe_create_link(
         &db,
@@ -43,6 +52,13 @@ async fn test_create_link_success() {
     assert_eq!(link.name, "Test Link");
     assert_eq!(link.value, "http://example.com");
     assert_eq!(link.created_by, None);
+
+    // Expect creation event
+    let event = events_rx.recv().await.unwrap();
+    assert!(matches!(
+        event,
+        TenantEventMessage::LinkCreated(created) if created.data.id == link.id
+    ));
 
     // Ensure the name is correctly indexed and searchable
     {
@@ -100,5 +116,106 @@ async fn test_create_link_success() {
         assert_eq!(first.total_hits, 1);
         assert!(!first.name_match);
         assert!(first.content_match);
+    }
+}
+
+/// Tests that a link can be deleted successfully
+#[tokio::test]
+async fn test_delete_link_success() {
+    let (_container_db, db) = create_test_tenant_database().await;
+    let (_container_search, search) = create_test_tenant_typesense().await;
+    let (events, mut events_rx) = MpscEventPublisher::new();
+    let events = TenantEventPublisher::Mpsc(events);
+    let (document_box, root) = create_document_box(
+        &db,
+        &events,
+        CreateDocumentBox {
+            scope: "test".to_string(),
+            created_by: None,
+        },
+    )
+    .await
+    .unwrap();
+
+    // Consume creation event
+    _ = events_rx.recv().await.unwrap();
+
+    let link = safe_create_link(
+        &db,
+        search.clone(),
+        &events,
+        CreateLinkData {
+            folder: root,
+            name: "Test Link".to_string(),
+            value: "http://example.com".to_string(),
+            created_by: None,
+        },
+    )
+    .await
+    .unwrap();
+
+    // Consume creation event
+    _ = events_rx.recv().await.unwrap();
+
+    // Ensure the correct data was inserted
+    assert_eq!(link.name, "Test Link");
+    assert_eq!(link.value, "http://example.com");
+    assert_eq!(link.created_by, None);
+
+    let link_id = link.id;
+
+    // Delete the link
+    delete_link(&db, &search, &events, link, document_box.scope.to_string())
+        .await
+        .unwrap();
+
+    // Expect deletion event
+    let event = events_rx.recv().await.unwrap();
+    assert!(matches!(
+        event,
+        TenantEventMessage::LinkDeleted(deleted) if deleted.data.id == link_id
+    ));
+
+    // Ensure the link cannot be found
+    {
+        let has_link = Link::find(&db, &document_box.scope, link_id)
+            .await
+            .unwrap()
+            .is_some();
+        assert!(!has_link);
+    }
+
+    // Ensure the name is correctly removed from the index and is not searchable
+    {
+        let request = SearchRequest {
+            query: Some("Test Link".to_string()),
+            include_name: true,
+            ..Default::default()
+        };
+
+        let result = search
+            .search_index(&["test".to_string()], request, None)
+            .await
+            .unwrap();
+
+        assert_eq!(result.total_hits, 0);
+        assert!(result.results.is_empty());
+    }
+
+    // Ensure the value is correctly removed from the index and is not searchable
+    {
+        let request = SearchRequest {
+            query: Some("http://example.com".to_string()),
+            include_content: true,
+            ..Default::default()
+        };
+
+        let result = search
+            .search_index(&["test".to_string()], request, None)
+            .await
+            .unwrap();
+
+        assert_eq!(result.total_hits, 0);
+        assert!(result.results.is_empty());
     }
 }
