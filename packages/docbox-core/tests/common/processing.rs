31ac79d27c4de53b608ee9056823cc8dedf656ad
Jacobtread <jacobtread@gmail.com>
2025-07-10 14:40:12 +1200

    fix: test container local startup delay

--- packages/docbox-core/tests/common/processing.rs
+++ packages/docbox-core/tests/common/processing.rs
@@ -1,3 +1,4 @@
+use anyhow::Context;
 use docbox_core::processing::{
     ProcessingLayer,
     office::{OfficeConverter, OfficeProcessingLayer, convert_server::OfficeConverterServer},
@@ -16,15 +17,25 @@ use testcontainers::{
 pub async fn create_processing_layer() -> (ProcessingLayer, ContainerAsync<GenericImage>) {
     let container = GenericImage::new("jacobtread/office-convert-server", "0.2.2")
         .with_exposed_port(3000.tcp())
+        .with_wait_for(WaitFor::seconds(5))
         .with_wait_for(WaitFor::http(
             HttpWaitStrategy::new("/status").with_expected_status_code(200u16),
         ))
         .start()
         .await
+        .context("crash on startup")
         .unwrap();
 
-    let host = container.get_host().await.unwrap();
-    let host_port = container.get_host_port_ipv4(3000).await.unwrap();
+    let host = container
+        .get_host()
+        .await
+        .context("crash on get host")
+        .unwrap();
+    let host_port = container
+        .get_host_port_ipv4(3000)
+        .await
+        .context("crash on get port")
+        .unwrap();
     let client_url = format!("http://{host}:{host_port}");
 
     let converter_server = OfficeConverterServer::from_addresses([client_url.as_str()]).unwrap();
