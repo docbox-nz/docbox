73faa3746bf872e48988feae38c2d19d7b4b504b
jacobtread <jacobtread@gmail.com>
2025-10-01 10:57:44 +1300

    feat: provide aws_config_with_profile

--- packages/docbox-core/src/aws.rs
+++ packages/docbox-core/src/aws.rs
@@ -16,3 +16,19 @@ pub async fn aws_config() -> SdkConfig {
         .load()
         .await
 }
+
+/// Create the AWS production configuration using a specific AWS_PROFILE
+pub async fn aws_config_with_profile(profile_name: impl Into<String>) -> SdkConfig {
+    let region_provider = RegionProviderChain::default_provider()
+        // Fallback to our desired region
+        .or_else("ap-southeast-2");
+
+    // Load the configuration from env variables (See https://docs.aws.amazon.com/sdkref/latest/guide/settings-reference.html#EVarSettings)
+    aws_config::from_env()
+        // Setup the region provider
+        .region(region_provider)
+        .profile_name(profile_name)
+        .behavior_version(BehaviorVersion::v2025_01_17())
+        .load()
+        .await
+}
