6a1ef5a3b4022a2e2bbe5b529b1c0da12338425c
Jacobtread <jacobtread@gmail.com>
2025-10-28 14:40:46 +1300

    feat: memory secret manager create test

--- packages/docbox-secrets/tests/test_create_secret.rs
+++ packages/docbox-secrets/tests/test_create_secret.rs
@@ -1,4 +1,4 @@
-use docbox_secrets::{Secret, SetSecretOutcome};
+use docbox_secrets::{Secret, SecretManager, SetSecretOutcome, memory::MemorySecretManager};
 
 use crate::common::aws::{test_aws_secrets_manager_client, test_loker_container};
 
@@ -8,6 +8,22 @@ mod common;
 async fn test_create_secret_aws_success() {
     let loker_container = test_loker_container().await;
     let secrets_manager = test_aws_secrets_manager_client(&loker_container).await;
+
+    // Should not have a secret
+    assert!(!secrets_manager.has_secret("test").await.unwrap());
+
+    // Should report a Created outcome
+    let outcome = secrets_manager.set_secret("test", "test").await.unwrap();
+    assert_eq!(outcome, SetSecretOutcome::Created);
+
+    // Should retrieve the same value
+    let value = secrets_manager.get_secret("test").await.unwrap();
+    assert_eq!(value, Some(Secret::String("test".to_string())));
+}
+
+#[tokio::test]
+async fn test_create_secret_memory_success() {
+    let secrets_manager = SecretManager::Memory(MemorySecretManager::default());
 
     // Should not have a secret
     assert!(!secrets_manager.has_secret("test").await.unwrap());
