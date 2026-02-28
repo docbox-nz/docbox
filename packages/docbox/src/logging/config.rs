ecf2cddf9f2e921d5e6fc846b701a377d3ec781e
Jacobtread <jacobtread@gmail.com>
2026-02-28 19:36:21 +1300

    feat: cloudwatch logging

--- packages/docbox/src/logging/config.rs
+++ packages/docbox/src/logging/config.rs
@@ -1,6 +1,7 @@
 use thiserror::Error;
 
 use crate::logging::{
+    cloudwatch::{CloudwatchLoggingConfig, CloudwatchLoggingConfigError},
     fmt::{LoggingFormatConfig, LoggingFormatConfigError},
     sentry::SentryLoggingConfig,
 };
@@ -10,18 +11,27 @@ use crate::logging::{
 pub struct LoggingConfig {
     pub format: LoggingFormatConfig,
     pub sentry: SentryLoggingConfig,
+    pub cloudwatch: CloudwatchLoggingConfig,
 }
 
 #[derive(Debug, Error)]
 pub enum LoggingConfigError {
     #[error(transparent)]
     LoggingFormatConfig(#[from] LoggingFormatConfigError),
+    #[error(transparent)]
+    CloudwatchLoggingConfig(#[from] CloudwatchLoggingConfigError),
 }
 
 impl LoggingConfig {
     pub fn from_env() -> Result<Self, LoggingConfigError> {
         let format = LoggingFormatConfig::from_env()?;
         let sentry = SentryLoggingConfig::from_env()?;
-        Ok(Self { format, sentry })
+        let cloudwatch = CloudwatchLoggingConfig::from_env()?;
+
+        Ok(Self {
+            format,
+            sentry,
+            cloudwatch,
+        })
     }
 }
